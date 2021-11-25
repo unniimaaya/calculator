5192932b4f914c1044bfe69c4f31c531175c8fb6
unnimaya <unnicu740@gmail.com>
2021-11-25 13:25:50 +0530

    snd commit

---
+++ script.js
@@ -0,0 +1,25 @@
+     
+    // function displayvalue(num){
+    //     let res=document.querySelector("#result")
+    //     res.value+=num
+    // }
+    // function clearbox(){
+    //     document.querySelector("#result").value=""
+    // }
+    // function evaluateExpression(){
+    //     let res=document.querySelector("#result")
+    //     let cur_val=res.value
+    //     let result=eval(cur_val)
+    //     res.value=result
+    // }
+    // function backSpace(){
+    //     let res=document.querySelector("#result")
+    //     let cur_val=res.value
+    //     res.value=cur_val.slice(0,1)
+    // }
+
+    var displayvalue=(num)=>result.value+=num
+
+    var clearBox=()=>result.value=""
+
+    var evaluateExpression=()=>result.value=eval(result.value)
