956fbc2b0a836c0bba2627d0534042c6eff007e6
unnimaya <unnicu740@gmail.com>
2021-11-28 21:25:05 +0530

    intial

--- script.js
+++ script.js
@@ -12,11 +12,11 @@
     //     let result=eval(cur_val)
     //     res.value=result
     // }
-    // function backSpace(){
-    //     let res=document.querySelector("#result")
-    //     let cur_val=res.value
-    //     res.value=cur_val.slice(0,1)
-    // }
+    function backSpace(){
+        let res=document.querySelector("#result")
+        let cur_val=res.value
+        res.value=cur_val.slice(0,1)
+    }
 
     var displayValue=(num)=>result.value+=num
 
